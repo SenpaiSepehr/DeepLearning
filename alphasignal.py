70e05552e48b64cbb03a6e57d58f3014a83454e4
Sepehr Nouri <54563010+SenpaiSepehr@users.noreply.github.com>
2023-10-11 16:51:27 +0900

    Create alphasignal.py

---
+++ alphasignal.py
@@ -0,0 +1,7 @@
+# 2D to 3D video reconstruction
+https://github.com/NVlabs/neuralangelo
+
+# Composer, using AI to manage trades and investments
+https://www.composer.trade/?utm_source=alphasignal&utm_medium=newsletter&utm_campaign=8-21-23
+
+

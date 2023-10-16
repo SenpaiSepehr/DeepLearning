9d75c5242fbf132739d0c23ae681c487ce25f635
Sepehr Nouri <54563010+SenpaiSepehr@users.noreply.github.com>
2023-10-16 17:29:53 +0900

    Update alphasignal.py

--- alphasignal.py
+++ alphasignal.py
@@ -1,7 +1,32 @@
+----- Papers
+1. Retentive Networks
+https://arxiv.org/abs/2307.08621
+
+2. Six lecutures on linearized neural nets
+https://arxiv.org/abs/2308.13431
+
+3. 
+
+----- Utility
 # 2D to 3D video reconstruction
 https://github.com/NVlabs/neuralangelo
 
 # Composer, using AI to manage trades and investments
 https://www.composer.trade/?utm_source=alphasignal&utm_medium=newsletter&utm_campaign=8-21-23
+
+# GPT pilot, code writing
+https://github.com/Pythagora-io/gpt-pilot#how-to-start-using-gpt-pilot
+
+# Open Interpreter, AI Language Model that runs code on local device
+https://github.com/KillianLucas/open-interpreter
+
+# OpenAI Translator
+https://github.com/openai-translator/openai-translator
+
+# 
+
+
+
+
 
 
